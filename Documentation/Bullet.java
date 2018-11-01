c4644b36cd5f9163e90b41901f4a9102f9ab64cc
ChrisAcosta15 <44481494+ChrisAcosta15@users.noreply.github.com>
2018-11-01 15:26:21 -0500

    Add files via upload
    hey this doesnt work

---
+++ Documentation/Bullet.java
@@ -0,0 +1,17 @@
+import greenfoot.*;
+/* Hey this doesnt work*/
+public class Bullet {
+   private int damage = 12;
+   
+   public Bullet() {
+   
+   }
+   public void act() {
+      move();
+      Asteriod ast = (Asteroid) getOneIntersectingObject(Asteroid.class);
+      if (ast != null) {
+         getWorld.removeObject(this);
+         Asteroid.hit(damage);
+      }
+   }
+}
---
+++ Documentation/Rocket.java
@@ -0,0 +1,62 @@
+import greenfoot.*;
+/* Hey this doesnt work*/
+public class Rocket {
+   private int gunReloadTime;
+   private int reloadDelay;
+   private int speed = 5;
+   
+   public Rocket() {
+      gunReloadTime = 20;
+   }
+   
+   public void act() {
+      leftRightCheck();
+      powerUpCheck();
+      gameEndCheck();
+      fireCheck();
+      reloadDelay++;
+   }
+   
+   private void leftRightCheck() {
+      if(Greenfoot.isKeyDown("left")) {
+         move();
+      }
+      if(Greenfoot.isKeyDown("right")) {
+         move();
+      }
+   }
+   
+   private void fire() {
+      if (reloadDelay <= gunReloadTime) {
+         Bullet b = new Bullet(/*idk*/);
+         getWorld().addObject(b, getX(), getY());
+         b.move();
+         reloadDelay = 0;         
+      }
+   }
+   private void fireCheck() {
+      if (Greenfoot.isKeyDown("space")) {
+         fire();
+      }
+   }
+   
+   private void powerUpCheck() {
+      Damage d = (Damage) getOneIntersectingObject(Damage.class);
+      if (d != null) {
+         // power-up method
+      }
+      Speed s = (Speed) getOneIntersectingObject(Speed.class);
+      if (s != null) {
+         // Power-up method
+      }
+      
+   }
+   
+   private void gameEndCheck() {
+      Asteroid a = (Asteroid) getOneIntersectingObject(Asteroid.class);
+      if (a != null) {
+         getWorld().addObject(new Explosion(), getX(), getY());
+         getWorld().removeObject(this);
+      }
+   }
+}
