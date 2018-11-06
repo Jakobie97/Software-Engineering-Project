87a764fb52204e64eef760b64763dfdf99e39c19
ChrisAcosta15 <44481494+ChrisAcosta15@users.noreply.github.com>
2018-11-06 16:07:55 -0600

    Update Asteroid.java
    Added the spawning of random powerups via the destruction of asteroids.

--- Asteroid.java
+++ Asteroid.java
@@ -8,6 +8,8 @@ import greenfoot.*;
  */
 public class Asteroid extends Actor
 {
+   private int size;
+   private int health;
 
     /**
      * Act - do whatever the Asteroid wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
@@ -20,6 +22,17 @@ public class Asteroid extends Actor
             Greenfoot.stop();
             
         }
+        for (Asteroid a : asteroids) {
+         if (health <= 0) {
+            int pwrUp = Greenfoot.getRandomNumber(9);
+            if (pwrUp < 2) {
+               addObject(new Speed(), a.getX(), a.getY()); 
+            }
+            if (pwrUp > 8) {
+               addObject(new Damage(), a.getX(), a.getY());
+            }
+            a.remove();
+         }
         
     }
 }
